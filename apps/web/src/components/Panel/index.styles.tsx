bd67b033aa9c88ebffb78224e640deeacfa4fa19
Sophia <me@sophia-dev.io>
2023-08-12 15:31:07 +0100

    fix scrolling problem for panel view

--- apps/web/src/components/Panel/index.styles.tsx
+++ apps/web/src/components/Panel/index.styles.tsx
@@ -3,8 +3,20 @@ import { Panel } from "react-resizable-panels";
 import { Paper } from "@mui/material";
 import styled from "@emotion/styled";
 
-export const Root = styled(Panel)``;
+export const Root = styled(Panel)`
+    max-height: 100%;
+
+    position: relative;
+`;
 
 export const Content = styled(Paper)`
     overflow: hidden;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    overflow-y: auto;
 `;
